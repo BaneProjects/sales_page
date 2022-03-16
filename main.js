6e1298a36e72c27a4da3a8e3b2f943386d5e7d64
Branislav Stojčić <banea380@gmail.com>
2022-03-16 14:37:21 +0100

    add accordion and small changes about heading and buttons

---
+++ main.js
@@ -0,0 +1,37 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.parentElement.parentElement.querySelector(".panel");
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+//the second accordion
+var acc_2 = document.getElementsByClassName("acc");
+var arrows = document.getElementsByClassName("roatate_arrow");
+var white_arr_mobile = document.getElementById("arrow_mobile");
+
+console.log(white_arr_mobile);
+for (i = 0; i < acc_2.length; i++) {
+  acc_2[i].addEventListener("click", function () {
+    var arrow = this.nextElementSibling;
+
+    console.log(arrow);
+    var el = this.parentElement.parentElement.querySelector(".open");
+
+    if (el.style.maxHeight) {
+      el.style.maxHeight = null;
+      arrow.style.transform = "rotate(0deg)";
+      white_arr_mobile.style.transform = "rotate(90deg)";
+    } else {
+      el.style.maxHeight = el.scrollHeight + "px";
+      arrow.style.transform = "rotate(270deg)";
+    }
+  });
+}
